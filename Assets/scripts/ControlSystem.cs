0d323f4a3097bc608dfaf06241d41adf40448592
xuanlinahua <131770644+xuanlinahua@users.noreply.github.com>
2023-05-23 21:46:59 +0800

    unity20230523後方代碼與物理碰狀

---
+++ Assets/scripts/ControlSystem.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ControlSystem : MonoBehaviour
+{
+    private void Awake()
+    {
+        print("C奶");
+        print("48*54883");
+        print($"{58745-99/885}");
+        print($"{58745-99}");
+    }
+    private void Start()
+    {
+        print("<color=yellow>開始事件</color>");
+    }
+    private void Update()
+    {
+        print("<color=#899887>更新事件</color>");
+    }
+}
---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
